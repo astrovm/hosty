0838d60b3423942ce3b5d415779895c13df6f8c9
astrolince <juan77.sonic@gmail.com>
2016-02-24 02:44:41 -0300

    juankfree to astrolince

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo wget -c https://github.com/juankfree/hosty/raw/master/hosty -O /usr/local/bin/hosty && sudo chmod +x /usr/local/bin/hosty
+sudo wget -c https://github.com/astrolince/hosty/raw/master/hosty -O /usr/local/bin/hosty && sudo chmod +x /usr/local/bin/hosty
