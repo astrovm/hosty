9b15917787f3706984b95026828d968423417290
astro <astro@astrolince.com>
2019-04-22 07:52:38 -0300

    install.sh with chmod 755

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo wget -c https://github.com/astrolince/hosty/raw/master/hosty -O /usr/local/bin/hosty && sudo chmod +x /usr/local/bin/hosty
+sudo wget -c https://github.com/astrolince/hosty/raw/master/hosty -O /usr/local/bin/hosty && sudo chmod 755 /usr/local/bin/hosty
