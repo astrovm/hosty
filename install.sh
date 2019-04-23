bd0d1e526d32ba5ef769bdc1ca3653775a2c27e2
astro <astro@astrolince.com>
2019-04-23 12:16:19 -0300

    adding echoes to install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-sudo wget -c https://github.com/astrolince/hosty/raw/master/hosty -O /usr/local/bin/hosty && sudo chmod 755 /usr/local/bin/hosty
+
+if [ -f /usr/local/bin/hosty ]; then
+    echo "Removing existing hosty..."
+    sudo rm /usr/local/bin/hosty
+    echo
+fi
+
+echo "Installing hosty..."
+sudo wget -c https://github.com/astrolince/hosty/raw/master/hosty -O /usr/local/bin/hosty
+echo
+
+echo "Fixing permissions..."
+sudo chmod 755 /usr/local/bin/hosty
+echo
+
+echo "Done."
