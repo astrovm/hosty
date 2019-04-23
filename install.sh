4b49cdaf189726297ecf305fcb335e765adfa590
astrolince <astro@astrolince.com>
2019-04-23 12:30:07 -0300

    adding sudo check to install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+prompt=$(sudo -nv 2>&1)
+if [ $? -eq 0 ]; then
+  # exit code of sudo-command is 0
+  echo "has_sudo__pass_set"
+elif echo $prompt | grep -q '^sudo:'; then
+  echo "has_sudo__needs_pass"
+else
+  echo "no_sudo"
+fi
+
 if [ -f /usr/local/bin/hosty ]; then
     echo "Removing existing hosty..."
     sudo rm /usr/local/bin/hosty
@@ -8,7 +18,6 @@ fi
 
 echo "Installing hosty..."
 sudo wget -c https://github.com/astrolince/hosty/raw/master/hosty -O /usr/local/bin/hosty
-echo
 
 echo "Fixing permissions..."
 sudo chmod 755 /usr/local/bin/hosty
