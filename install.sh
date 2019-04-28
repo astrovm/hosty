d77b72afba15fdf3e578d5545d9c984a69b8df5f
astrolince <astro@astrolince.com>
2019-04-28 09:39:47 -0300

    require all hosty mandatory dependencies

--- install.sh
+++ install.sh
@@ -1,13 +1,18 @@
-#!/bin/bash
+#!/bin/sh
+
+command -v sudo >/dev/null 2>&1 || { echo >&2 "Hosty requires 'sudo' but it's not installed."; exit 1; }
+command -v grep >/dev/null 2>&1 || { echo >&2 "Hosty requires 'grep' but it's not installed."; exit 1; }
+command -v curl >/dev/null 2>&1 || { echo >&2 "Hosty requires 'curl' but it's not installed."; exit 1; }
+command -v chmod >/dev/null 2>&1 || { echo >&2 "Hosty requires 'chmod' but it's not installed."; exit 1; }
+command -v bash >/dev/null 2>&1 || { echo >&2 "Hosty requires 'bash' but it's not installed."; exit 1; }
+command -v gpg >/dev/null 2>&1 || { echo >&2 "Hosty requires 'gpg' but it's not installed."; exit 1; }
+command -v awk >/dev/null 2>&1 || { echo >&2 "Hosty requires 'awk' but it's not installed."; exit 1; }
+command -v head >/dev/null 2>&1 || { echo >&2 "Hosty requires 'head' but it's not installed."; exit 1; }
+command -v cat >/dev/null 2>&1 || { echo >&2 "Hosty requires 'cat' but it's not installed."; exit 1; }
 
 echo "======== Welcome to hosty installer ========"
 echo "========    astrolince.com/hosty    ========"
 echo
-
-command -v sudo >/dev/null 2>&1 || { echo >&2 "The installer requires 'sudo' but it's not installed."; exit 1; }
-command -v grep >/dev/null 2>&1 || { echo >&2 "The installer requires 'grep' but it's not installed."; exit 1; }
-command -v curl >/dev/null 2>&1 || { echo >&2 "The installer requires 'curl' but it's not installed."; exit 1; }
-command -v chmod >/dev/null 2>&1 || { echo >&2 "The installer requires 'chmod' but it's not installed."; exit 1; }
 
 # Checking sudo
 echo "Checking if user has sudo access..."
@@ -56,11 +61,7 @@ echo "Fixing permissions..."
 sudo chmod 755 /usr/local/bin/hosty
 echo
 
-echo "Checking dependencies..."
-command -v gpg >/dev/null 2>&1 || { echo >&2 "Hosty requires 'gpg' but it's not installed."; }
-command -v awk >/dev/null 2>&1 || { echo >&2 "Hosty requires 'awk' but it's not installed."; }
-command -v head >/dev/null 2>&1 || { echo >&2 "Hosty requires 'head' but it's not installed."; }
-command -v cat >/dev/null 2>&1 || { echo >&2 "Hosty requires 'cat' but it's not installed."; }
+echo "Checking optional dependencies..."
 command -v zcat >/dev/null 2>&1 || { echo >&2 "Hosty can require 'zcat' if you want to use custom .zip sources, but it's not installed."; }
 command -v 7z >/dev/null 2>&1 || { echo >&2 "Hosty can require '7z' if you want to use custom .7z sources, but it's not installed."; }
 echo
