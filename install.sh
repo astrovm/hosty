e140471c92c49004623a4731355603338c0698c7
astro <astro@astrolince.com>
2020-07-20 11:09:12 -0300

    removing sudo requirement from installer

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 echo
-command -v sudo >/dev/null 2>&1 || { echo >&2 "Hosty requires 'sudo' but it's not installed."; exit 1; }
 command -v grep >/dev/null 2>&1 || { echo >&2 "Hosty requires 'grep' but it's not installed."; exit 1; }
 command -v curl >/dev/null 2>&1 || { echo >&2 "Hosty requires 'curl' but it's not installed."; exit 1; }
 command -v chmod >/dev/null 2>&1 || { echo >&2 "Hosty requires 'chmod' but it's not installed."; exit 1; }
@@ -16,18 +15,19 @@ echo "======== Welcome to hosty installer ========"
 echo "========    astrolince.com/hosty    ========"
 echo
 
-# Checking sudo
-echo "Checking if user has sudo access..."
+# Checking root
+echo "Checking if user has root access..."
 
 prompt=$(sudo -nv 2>&1)
 
-if [ $? -eq 0 ]; then
+if [ $? -eq 0 ] || [ "$EUID" -eq 0 ]; then
     echo "OK"
     echo
+    MainHosty
 elif echo $prompt | grep -q '^sudo:'; then
     echo
     echo "Requesting sudo..."
-    sudo -v
+    sudo MainHosty
     echo
 else
     echo
@@ -35,50 +35,52 @@ else
     exit 1
 fi
 
-if [ -f /usr/local/bin/hosty ]; then
-    echo "Removing existing hosty..."
-    sudo rm /usr/local/bin/hosty
+MainHosty () {
+    if [ -f /usr/local/bin/hosty ]; then
+        echo "Removing existing hosty..."
+        rm /usr/local/bin/hosty
+        echo
+    fi
+    
+    echo "Do you want to always run the latest version of hosty? (recommended) y/n"
+    read answer < /dev/tty
     echo
-fi
-
-echo "Do you want to always run the latest version of hosty? (recommended) y/n"
-read answer < /dev/tty
-echo
-
-# Check user answer
-if [ "$answer" = "y" ]; then
-    echo "Installing hosty..."
-    sudo curl -L -o /usr/local/bin/hosty https://raw.githubusercontent.com/astrolince/hosty/master/updater.sh
+    
+    # Check user answer
+    if [ "$answer" = "y" ]; then
+        echo "Installing hosty..."
+        curl -L -o /usr/local/bin/hosty https://raw.githubusercontent.com/astrolince/hosty/master/updater.sh
+        echo
+    elif [ "$answer" = "n" ]; then
+        echo "Installing hosty..."
+        curl -L -o /usr/local/bin/hosty https://raw.githubusercontent.com/astrolince/hosty/master/hosty.sh
+        echo
+    else
+        echo "Bad answer, exiting..."
+        exit 1
+    fi
+    
+    echo "Fixing permissions..."
+    chmod 755 /usr/local/bin/hosty
     echo
-elif [ "$answer" = "n" ]; then
-    echo "Installing hosty..."
-    sudo curl -L -o /usr/local/bin/hosty https://raw.githubusercontent.com/astrolince/hosty/master/hosty.sh
+    
+    echo "Checking optional dependencies..."
+    command -v zcat >/dev/null 2>&1 || { echo >&2 "Hosty can require 'zcat' if you want to use custom .zip sources, but it's not installed."; }
+    command -v 7z >/dev/null 2>&1 || { echo >&2 "Hosty can require '7z' if you want to use custom .7z sources, but it's not installed."; }
     echo
-else
-    echo "Bad answer, exiting..."
-    exit 1
-fi
-
-echo "Fixing permissions..."
-sudo chmod 755 /usr/local/bin/hosty
-echo
-
-echo "Checking optional dependencies..."
-command -v zcat >/dev/null 2>&1 || { echo >&2 "Hosty can require 'zcat' if you want to use custom .zip sources, but it's not installed."; }
-command -v 7z >/dev/null 2>&1 || { echo >&2 "Hosty can require '7z' if you want to use custom .7z sources, but it's not installed."; }
-echo
-
-echo "Do you want to automatically update your hosts file with latest ads list? (recommended) y/n"
-read answer < /dev/tty
-echo
-
-# Check user answer
-if [ "$answer" = "y" ]; then
-    sudo /usr/local/bin/hosty --autorun < /dev/tty
-    exit 0
-elif [ "$answer" != "n" ]; then
-    echo "Bad answer, exiting..."
-    exit 1
-fi
-
-echo "Done."
+    
+    echo "Do you want to automatically update your hosts file with latest ads list? (recommended) y/n"
+    read answer < /dev/tty
+    echo
+    
+    # Check user answer
+    if [ "$answer" = "y" ]; then
+        /usr/local/bin/hosty --autorun < /dev/tty
+        exit 0
+    elif [ "$answer" != "n" ]; then
+        echo "Bad answer, exiting..."
+        exit 1
+    fi
+    
+    echo "Done."
+}
