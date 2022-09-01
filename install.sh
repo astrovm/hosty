4a56b5d309b7b4ec159e3399d5a7cd6e6e08dc9b
astrolince <astro@astrolince.com>
2022-09-01 15:36:12 -0300

    simplifying scripts

--- install.sh
+++ install.sh
@@ -2,91 +2,69 @@
 
 set -euf
 
-echo
-
-# Check dependences
+# check dependences
 checkDep() {
     command -v "$1" >/dev/null 2>&1 || {
-        echo >&2 "Hosty requires '$1' but it's not installed."
+        echo >&2 "hosty requires '$1' but it's not installed."
         exit 1
     }
 }
 
 checkDep curl
 
-# Define main function
-mainHosty() {
-    echo
-
-    if [ -f /usr/local/bin/hosty ]; then
-        echo "Removing existing hosty..."
-        $1 rm /usr/local/bin/hosty
-        echo
-    fi
-
-    echo "Do you want to always run the latest version of hosty? (recommended) y/n"
-    read -r answer </dev/tty
-    echo
-
-    # Check user answer
-    if [ "$answer" = "y" ]; then
-        echo "Installing hosty..."
-        $1 curl -L -o /usr/local/bin/hosty https://raw.githubusercontent.com/astrolince/hosty/master/updater.sh
-        echo
-    elif [ "$answer" = "n" ]; then
-        echo "Installing hosty..."
-        $1 curl -L -o /usr/local/bin/hosty https://raw.githubusercontent.com/astrolince/hosty/master/hosty.sh
-        echo
-    else
-        echo "Bad answer, exiting..."
-        exit 1
-    fi
-
-    echo "Fixing permissions..."
-    $1 chmod 755 /usr/local/bin/hosty
-    echo
-
-    echo "Do you want to automatically update your hosts file with latest ads list? (recommended) y/n"
-    read -r answer </dev/tty
-    echo
-
-    # Check user answer
-    if [ "$answer" = "y" ]; then
-        $1 /usr/local/bin/hosty --autorun </dev/tty
-        exit 0
-    elif [ "$answer" != "n" ]; then
-        echo "Bad answer, exiting..."
-        exit 1
-    fi
-
-    echo "Done."
-}
-
-# Start script
-echo "======== Welcome to hosty installer ========"
+# start script
+echo "======== welcome to hosty installer ========"
 echo "========    astrolince.com/hosty    ========"
 echo
 
-# Check root and exec main function as it
-echo "Checking if user has root access..."
+# check root
+echo "checking if user has root access..."
+echo
 
 if [ "$(id -u)" != 0 ]; then
-    echo
-
-    if ! prompt=$(sudo -nv 2>&1); then
-        if ! echo "$prompt" | grep -q '^sudo:'; then
-            echo "You don't have sudo access, please fix that or run it from root."
-            exit 1
-        fi
-
-        echo "Requesting sudo..."
-    else
-        echo "Using already granted sudo access..."
-    fi
-
-    mainHosty sudo
-    exit 0
+    echo "you don't have root access, run the installer with sudo or from root."
+    exit 1
 fi
 
-echo "OK"
-mainHosty
+if [ -f /usr/local/bin/hosty ]; then
+    echo "removing existing hosty..."
+    rm /usr/local/bin/hosty
+    echo
+fi
+
+echo "do you want to always run the latest version of hosty? (recommended) y/n"
+read -r answer </dev/tty
+echo
+
+# check user answer
+if [ "$answer" = "y" ] || [ "$answer" = "Y" ] || [ "$answer" = "yes" ] || [ "$answer" = "YES" ]; then
+    echo "installing hosty..."
+    curl -L -o /usr/local/bin/hosty https://raw.githubusercontent.com/astrolince/hosty/master/updater.sh
+    echo
+elif [ "$answer" = "n" ] || [ "$answer" = "N" ] || [ "$answer" = "no" ] || [ "$answer" = "NO" ]; then
+    echo "installing hosty..."
+    curl -L -o /usr/local/bin/hosty https://raw.githubusercontent.com/astrolince/hosty/master/hosty.sh
+    echo
+else
+    echo "bad answer, exiting..."
+    exit 1
+fi
+
+echo "fixing permissions..."
+chmod 755 /usr/local/bin/hosty
+echo
+
+echo "do you want to automatically update your hosts file with latest ads list? (recommended) y/n"
+read -r answer </dev/tty
+echo
+
+# check user answer
+if [ "$answer" = "y" ]; then
+    /usr/local/bin/hosty --autorun </dev/tty
+    exit 0
+elif [ "$answer" != "n" ]; then
+    echo "bad answer, exiting..."
+    exit 1
+fi
+
+echo "done."
--- updater.sh
+++ updater.sh
@@ -2,10 +2,10 @@
 
 set -euf
 
-# Check dependences
+# check dependences
 checkDep() {
     command -v "$1" >/dev/null 2>&1 || {
-        echo >&2 "Hosty requires '$1' but it's not installed."
+        echo >&2 "hosty requires '$1' but it's not installed."
         exit 1
     }
 }
@@ -14,32 +14,32 @@ checkDep curl
 checkDep gpg
 checkDep mktemp
 
-# Creating tmp files
+# creating tmp files
 astrokeys=$(mktemp)
 hosty=$(mktemp)
 signature=$(mktemp)
 
-# Download function
+# download function
 downloadFiles() {
     if ! curl -H 'Cache-Control: no-cache' -fsSL -o "$1" "$2"; then
-        echo "Error downloading $2"
+        echo "error downloading $2"
         rm "$astrokeys" "$hosty" "$signature"
         exit 1
     fi
 }
 
-# Download files
+# download files
 downloadFiles "$astrokeys" https://keybase.io/astrolince/pgp_keys.asc
 downloadFiles "$hosty" https://raw.githubusercontent.com/astrolince/hosty/master/hosty.sh
 downloadFiles "$signature" https://raw.githubusercontent.com/astrolince/hosty/master/hosty.sh.sig
 
-# Verify signature
+# verify signature
 gpg --dearmor "$astrokeys" >/dev/null 2>&1
 
-# If there is a problem, kill the program
+# if there is a problem, kill the program
 if ! gpg --no-default-keyring --keyring "$astrokeys.gpg" --verify "$signature" "$hosty" >/dev/null 2>&1; then
     rm "$astrokeys" "$hosty" "$signature" "$astrokeys.gpg"
-    echo "There is a problem with the signature, probably hosty repository was compromised, no changes were made to your system."
+    echo "there is a problem with the signature, probably hosty repository was compromised, no changes were made to your system."
     exit 1
 fi
 
