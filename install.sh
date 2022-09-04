7085fc3dc4008ed31e9d349fb53a88dc1fd9b7f5
astrolince <astro@astrolince.com>
2022-09-04 19:56:17 -0300

    restore autorun config in the installer

--- install.sh
+++ install.sh
@@ -50,4 +50,20 @@ fi
 echo "fixing permissions..."
 chmod 755 /usr/local/bin/hosty
 echo
+
+if command -v "crontab" >/dev/null 2>&1; then
+    echo "do you want to automatically update your hosts file with the latest ads list? (recommended) y/n"
+    read -r answer </dev/tty
+    echo
+
+    # check user answer
+    if [ "$answer" = "y" ] || [ "$answer" = "Y" ] || [ "$answer" = "yes" ] || [ "$answer" = "YES" ]; then
+        /usr/local/bin/hosty -a </dev/tty
+        exit 0
+    elif [ "$answer" != "n" ] && [ "$answer" != "N" ] && [ "$answer" != "no" ] && [ "$answer" != "NO" ]; then
+        echo "bad answer, exiting..."
+        exit 1
+    fi
+fi
+
 echo "done."
