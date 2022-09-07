3f37222a53203c5d9db23e203cb4c16ae7af41dd
astro <astro@astrolince.com>
2022-09-07 01:31:33 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -32,14 +32,13 @@ if [ "$(id -u)" != 0 ]; then
 
     REQUEST_SUDO=1
 else
-    REQUEST_SUDO=0
     echo "OK"
 fi
 
 echo
 if [ -f /usr/local/bin/hosty ]; then
     echo "removing existing hosty..."
-    if [ "$REQUEST_SUDO" = 1 ]; then
+    if [ "$REQUEST_SUDO" ]; then
         sudo rm /usr/local/bin/hosty
     else
         rm /usr/local/bin/hosty
@@ -53,7 +52,7 @@ echo
 
 if [ "$answer" = "y" ] || [ "$answer" = "Y" ] || [ "$answer" = "yes" ] || [ "$answer" = "YES" ]; then
     echo "installing hosty..."
-    if [ "$REQUEST_SUDO" = 1 ]; then
+    if [ "$REQUEST_SUDO" ]; then
         sudo curl -L -o /usr/local/bin/hosty https://raw.githubusercontent.com/astrolince/hosty/master/updater.sh
     else
         curl -L -o /usr/local/bin/hosty https://raw.githubusercontent.com/astrolince/hosty/master/updater.sh
@@ -61,7 +60,7 @@ if [ "$answer" = "y" ] || [ "$answer" = "Y" ] || [ "$answer" = "yes" ] || [ "$an
     echo
 elif [ "$answer" = "n" ] || [ "$answer" = "N" ] || [ "$answer" = "no" ] || [ "$answer" = "NO" ]; then
     echo "installing hosty..."
-    if [ "$REQUEST_SUDO" = 1 ]; then
+    if [ "$REQUEST_SUDO" ]; then
         sudo curl -L -o /usr/local/bin/hosty https://raw.githubusercontent.com/astrolince/hosty/master/hosty.sh
     else
         curl -L -o /usr/local/bin/hosty https://raw.githubusercontent.com/astrolince/hosty/master/hosty.sh
@@ -73,7 +72,7 @@ else
 fi
 
 echo "fixing permissions..."
-if [ "$REQUEST_SUDO" = 1 ]; then
+if [ "$REQUEST_SUDO" ]; then
     sudo chmod 755 /usr/local/bin/hosty
 else
     chmod 755 /usr/local/bin/hosty
@@ -86,7 +85,7 @@ if command -v "crontab" >/dev/null 2>&1; then
     echo
 
     if [ "$answer" = "y" ] || [ "$answer" = "Y" ] || [ "$answer" = "yes" ] || [ "$answer" = "YES" ]; then
-        if [ "$REQUEST_SUDO" = 1 ]; then
+        if [ "$REQUEST_SUDO" ]; then
             # shellcheck disable=SC2024
             sudo /usr/local/bin/hosty -a </dev/tty
         else
