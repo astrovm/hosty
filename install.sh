ada3bcfbd70d5e60a7a1adec817662afce7445af
astro <github.u3vwv@passfwd.com>
2025-08-22 23:22:18 -0300

    v1.9.9: update release date

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ set -euf
 
 # check dependences
 checkDep() {
-    command -v "$1" >/dev/null 2>&1 || {
+    command -v "$1" > /dev/null 2>&1 || {
         echo >&2 "hosty requires '$1' but it's not installed."
         exit 1
     }
@@ -30,16 +30,16 @@ if [ "$(id -u)" != 0 ]; then
         echo "using already granted sudo access..."
     fi
 
-    REQUEST_SUDO=1
+    request_sudo=1
 else
-    REQUEST_SUDO=0
+    request_sudo=0
     echo "OK"
 fi
 
 echo
 if [ -f /usr/local/bin/hosty ]; then
     echo "removing existing hosty..."
-    if [ "$REQUEST_SUDO" = 1 ]; then
+    if [ "$request_sudo" = 1 ]; then
         sudo rm /usr/local/bin/hosty
     else
         rm /usr/local/bin/hosty
@@ -48,7 +48,7 @@ if [ -f /usr/local/bin/hosty ]; then
 fi
 
 echo "installing hosty..."
-if [ "$REQUEST_SUDO" = 1 ]; then
+if [ "$request_sudo" = 1 ]; then
     sudo curl -L --retry 3 -o /usr/local/bin/hosty https://4st.li/hosty/hosty.sh
 else
     curl -L --retry 3 -o /usr/local/bin/hosty https://4st.li/hosty/hosty.sh
@@ -56,24 +56,24 @@ fi
 echo
 
 echo "fixing permissions..."
-if [ "$REQUEST_SUDO" = 1 ]; then
+if [ "$request_sudo" = 1 ]; then
     sudo chmod 755 /usr/local/bin/hosty
 else
     chmod 755 /usr/local/bin/hosty
 fi
 echo
 
-if command -v "crontab" >/dev/null 2>&1; then
+if command -v "crontab" > /dev/null 2>&1; then
     echo "do you want to automatically update your hosts file with the latest ads list? (recommended) y/n"
-    read -r answer </dev/tty
+    read -r answer < /dev/tty
     echo
 
     if [ "$answer" = "y" ] || [ "$answer" = "Y" ] || [ "$answer" = "yes" ] || [ "$answer" = "YES" ]; then
-        if [ "$REQUEST_SUDO" = 1 ]; then
+        if [ "$request_sudo" = 1 ]; then
             # shellcheck disable=SC2024
-            sudo /usr/local/bin/hosty -a </dev/tty
+            sudo /usr/local/bin/hosty -a < /dev/tty
         else
-            /usr/local/bin/hosty -a </dev/tty
+            /usr/local/bin/hosty -a < /dev/tty
         fi
         exit 0
     elif [ "$answer" != "n" ] && [ "$answer" != "N" ] && [ "$answer" != "no" ] && [ "$answer" != "NO" ]; then
